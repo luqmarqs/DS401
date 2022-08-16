0be34eeac2e84edb48db6db0c5b002444c1edf2f
Lucas Marques <66765396+luqmarqs@users.noreply.github.com>
2022-08-16 19:28:43 -0300

    Update bibliotecaRecursiva.java
    mais algumas funções

--- bibliotecaRecursiva.java
+++ bibliotecaRecursiva.java
@@ -5,22 +5,65 @@ public class Main
     }
     public static boolean isNegativoAux(int a, int d){
         if(a==0)
-            return false;
-        if(d==0)
             return true;
+        if(d==0)
+            return false;
         a++;
         d--;
         return isNegativoAux(a, d);
     }
 
     public static int soma (int a, int b){
-        
+        if(a==0)
+            return b;
+        if(isNegativo(a)){
+            b--;
+            a++;
+        }
+        if(!isNegativo(a)){
+            a--;
+            b++;
+        }
+        return soma(a,b);
+    }
+    
+    public static int subtracao(int a, int b){
+        if(a==0)
+            return b;
+        if(isNegativo(a)){
+            b++;
+            a++;
+        }
+        if(!isNegativo(a)){
+            a--;
+            b--;
+        }
+        return subtracao(a,b);
     }
   
+    public static boolean maiorQue (a, b){
+        
+    }
+    
+    public static boolean menorQue (a, b) {
+        
+    }
+    
+    public static boolean maiorIgualQue (a, b){
+        
+    }
+    
+    public static boolean menorIgualQue (a, b) {
+        
+    }
+    
     
     
 	public static void main(String[] args) {
 		System.out.println(isNegativo(-5));
+        System.out.println(soma(5, -3));
+        System.out.println(subtracao(5, -3));
+
 	}
 }
 //fazer: soma, subtracao, equidade, nao-equidade, maior que, maior igual, menor que, menor igual, multiplicacao e divisao
